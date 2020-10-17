78ae1eabf1159ab6d6a0f868b46cf863d52a7717
Michael Mims <michael.x.mims@gsk.com>
2020-10-17 14:52:04 -0400

    add initial commands structure for prompt

--- commands/commands.go
+++ commands/commands.go
@@ -1,1 +1,42 @@
 package commands
+
+import (
+	"github.com/c-bata/go-prompt"
+)
+
+type Commands struct {
+	MainSuggestions []prompt.Suggest
+	SubSuggestions  map[string][]prompt.Suggest
+}
+
+func New() Commands {
+	return Commands{
+		MainSuggestions: []prompt.Suggest{
+			{Text: "confluence", Description: "Confluence documentation for EDAP"},
+			{Text: "coretech", Description: "CoreTech releated resources"},
+			{Text: "github", Description: "Github EDAP repoistories"},
+			{Text: "jira", Description: "JIRA releated stories and release pages"},
+			{Text: "rps", Description: "RPS client related links"},
+		},
+		SubSuggestions: map[string][]prompt.Suggest{
+			"github": {
+				prompt.Suggest{Text: "Mdmims", Description: "Personal Github Repo"},
+			},
+			"go": {
+				prompt.Suggest{Text: "Docs", Description: "Golang documentation"},
+				prompt.Suggest{Text: "StandardLib", Description: "Golang standard library docs"},
+			},
+			"python": {
+				prompt.Suggest{Text: "StandardLib", Description: "Python standard library docs"},
+			},
+		},
+	}
+}
+func (c *Commands) GetMainSuggestions() []prompt.Suggest {
+	return c.MainSuggestions
+}
+
+func (c *Commands) GetSubSuggestions(key string) []prompt.Suggest {
+	sub := c.SubSuggestions
+	return sub[key]
+}
